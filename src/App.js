75a0ab54a556467a31087bde3563461eca65be14
axlbert <aclanger@hotmail.de>
2018-06-10 22:32:46 +0200

    custom input fields and default values defined

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import RegisterForm from './components/RegisterForm';
 class RegisterFormContainer extends Component {
   submit = values => {
     window.alert(JSON.stringify(values, null, 4));
-  }
+  };
+
+
+  getInitialValues() {
+    return {
+      preference : 'spaces',
+      newsletter : 'true'
+    }
+  };
+
   render () {
     return (
-      <RegisterForm onSubmit={this.submit}/>
+      <RegisterForm onSubmit={this.submit} initialValues={this.getInitialValues()}/>
       )
   }
 }
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import {customInput, customSelect } from './fields';
 
 class RegisterForm extends Component {
 	render() {
@@ -7,26 +8,28 @@ class RegisterForm extends Component {
 		const { handleSubmit} = this.props;
 		return(
 			<form onSubmit={handleSubmit}>
-				<div>
-					<label>Name</label>
-					<Field name="name" component="input" type="text" />
-					</div>
-				<div>
-					<label>Preferred Formatting</label>
-					<Field name="preference" component="select">
-						<option/>
-						<option value="tabs">Tabs</option>
-						<option value="spaces">Spaces</option>
-					</Field>
-				</div>
-				<div>
-					<label>Sign up to newsletter?</label>
-					<Field
-						name="newsletter"
-						component="input"
-						type="checkbox"
+				
+					<Field 
+						name="name"
+						component={customInput}
+						type="text"
+						label="Name" 
 						/>
-				</div>
+					
+				
+					<Field 
+						name="preference"
+						component={customSelect}
+						label="Preferred Formatting"
+						/>
+				
+				<Field 
+						name="newsletter"
+						component={customInput}
+						type="checkbox"
+						label="Sign up to Newsletter?" 
+						/>
+
 				<button type="submit">Submit</button>
 
 			</form>
